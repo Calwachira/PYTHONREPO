8bb0c51298fe5f9f5a9872cc836e296a43671677
Calwachira <wachiracaleb908@gmail.com>
2026-04-25 14:10:34 +0300

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,24 @@
+print("Enter your name:")
+name: str = input()
+
+
+is_logged_in = True
+if is_logged_in:
+ print(f"Hello {name}! ")
+
+
+grade = int(input("Please Enter your score : "))
+
+if grade >= 90:
+    print(f"Congratulations  {name}!  your grade is A")
+elif grade >= 80:
+    print(f"Congratulations  {name}!  your grade is B")
+elif grade >= 70:
+    print(f"Congratulations  {name}!  your grade is C")
+elif grade >= 60:
+    print(f"Congratulations  {name}!  your grade is D")
+
+
+
+
+
