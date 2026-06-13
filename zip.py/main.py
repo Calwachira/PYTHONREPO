1fd9d11c470a972baebc05fea8b2a400a935abb6
Calwachira <wachiracaleb908@gmail.com>
2026-06-13 09:19:02 +0300

    Add files via upload

---
+++ zip.py/main.py
@@ -0,0 +1,6 @@
+developers = ['Kyle', 'Chris', 'Joy', 'Aristle', 'Oprah','Jane']
+ids = [1, 2, 3, 4, 5, 6]
+
+for name, id in zip(developers, ids):
+    print(f'Name: {name}')
+    print(f'ID: {id}')
